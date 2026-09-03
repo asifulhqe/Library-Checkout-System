9f08635844d3ae6b14f865dce48fd276f13e5206
Asif <asifulhqe@gmail.com>
2026-09-03 14:27:58 +0100

    Fixed init of all classes, Added simple test

--- Library Checkout.py
+++ Library Checkout.py
@@ -1,11 +1,15 @@
 class Book:
     def __init__(self, title, author, isbn):
-        title = self.title
-        author = self.author
-        isbn = self.isbn
+        self.title = title
+        self.author = author
+        self.isbn = isbn
 
 class Member:
     def __init__(self, name, id):
-        name = self.name
-        id = self.id
+        self.name = name
+        self.id = id
 
+#Test
+Book1 = Book("Atomic", "John", 123456789)
+Book1.title = "Habits"
+print(Book1.title)
